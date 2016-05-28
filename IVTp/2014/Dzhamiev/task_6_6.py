dacd35460e73a87433250c2ba2a58cb7bc20270a
Dzhamal <dzh96@mail.ru>
2016-05-28 23:21:52 +0300

    Add files via upload

---
+++ IVTp/2014/Dzhamiev/task_6_6.py
@@ -0,0 +1,46 @@
+# Задача 6. Вариант 6.
+# Создайте игру, в которой компьютер загадывает название одного из семи городов России, имеющих действующий метрополитен, а игрок должен его угадать.
+
+# Dzhamiev D. R.
+# 11.04.2016
+
+
+from random import choice
+
+print('Угадайте один из семи городов России, имеющий действующий метрополитен!')
+
+cities = tuple('Москва Петербург Новгород Новосибирск Самара Екатеринбург Казань' .split())
+
+cities = [x.lower() for x in cities]
+
+right = choice(cities)
+trys = []
+
+
+k = 1
+while True:
+    print('\n', k, 'попытка..')
+    word = input('> ').lower()
+    
+    if right == word:
+        print('Правильно! Это', right + '!')
+        input('\nНамите Enter...')
+        break
+    
+    if word not in cities:
+        print('Такого города нет')
+        continue
+    
+    if word in trys:
+        print('Вы уже пробовали', word)
+        continue
+    trys.append(word)
+    
+    k += 1
+    if k == 4:
+        print('Подсказка: последниие две буквы -', right[-2:])
+        
+    elif k == 6:
+        r = cities.index(right)
+        print('Подсказка:', cities[r-1], '...',
+              cities[r+1 if r < len(cities)-1 else 0])
